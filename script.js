8428a983494e2210c9b4871802a361d9cfc17416
ezhang7423 <54998055+ezhang7423@users.noreply.github.com>
2019-09-09 22:04:48 -0700

    added buttondown effect, added bkspc and clear eff
    todo: everything in the last commit, also when calc is in focus make keyboard events work

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 
 let consoView = document.querySelector('.consoleArea')
 
-let currentStack = 0
+let currentStack = ''
+
 
 
 for (let x = 0; x<10; x++){
 	document.querySelector(`.d${x}`).addEventListener('click', () =>
-	 {currentStack = currentStack*10+x; 
+	 {currentStack = currentStack.concat(x); 
 		consoView.innerText = currentStack
 	})
 }
 
+
 let clear = document.querySelector('.clear')
-clear.addEventListener('click', () => {currentStack = 0; 
+clear.addEventListener('click', () => {currentStack = ''; 
 	consoView.innerText = 0})
+
+let bkspc = document.querySelector('.bkspc')
+bkspc.addEventListener('click', () => {currentStack = currentStack.slice(0, currentStack.length-1);
+	consoView.innerText = consoView.innerText.slice(0, consoView.innerText.length-1)
+	if (consoView.innerText == '') consoView.innerText = '0'})
 
