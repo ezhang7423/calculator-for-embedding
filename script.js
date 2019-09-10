0e9c86ffcea978914ba8af2094ecd2385eb47add
ezhang7423 <54998055+ezhang7423@users.noreply.github.com>
2019-09-10 08:14:00 -0700

    v1! mostly working
    so the caculator works(mostly). hurrah
    
    so many new features to create....
    history(massive feature) for expressions and answers
    variables
    decimals
    keyboard support
    make font size smaller as expression grows(or increase view)
    support ans shortcut
    create custom error message
    make x( = x*( and )x = )*x

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let consoView = document.querySelector('.consoleArea')
 let currentStack = ''
 
 
+function zaddCharToStackAndView(char) {
+	currentStack = currentStack.concat(char)
+	consoView.innerText = currentStack
+};
 
 for (let x = 0; x<10; x++){
 	document.querySelector(`.d${x}`).addEventListener('click', () =>
@@ -13,12 +17,63 @@ for (let x = 0; x<10; x++){
 }
 
 
-let clear = document.querySelector('.clear')
-clear.addEventListener('click', () => {currentStack = ''; 
+document.querySelector('.clear').addEventListener('click', () => {currentStack = ''; 
 	consoView.innerText = 0})
 
-let bkspc = document.querySelector('.bkspc')
-bkspc.addEventListener('click', () => {currentStack = currentStack.slice(0, currentStack.length-1);
+document.querySelector('.bkspc').addEventListener('click', () => {currentStack = currentStack.slice(0, currentStack.length-1);
 	consoView.innerText = consoView.innerText.slice(0, consoView.innerText.length-1)
 	if (consoView.innerText == '') consoView.innerText = '0'})
 
+
+document.querySelector('.exponent').addEventListener('click', () => {
+	if (isNaN(parseInt(currentStack.charAt(currentStack.length-1))) == false){
+				currentStack = currentStack.concat('^')
+				consoView.innerText = currentStack}
+})
+
+document.querySelector('.divide').addEventListener('click', () => {
+	if (isNaN(parseInt(currentStack.charAt(currentStack.length-1))) == false){
+				currentStack = currentStack.concat('/')
+				consoView.innerText = currentStack}
+})
+
+document.querySelector('.multiply').addEventListener('click', () => {
+	if (isNaN(parseInt(currentStack.charAt(currentStack.length-1))) == false){
+				currentStack = currentStack.concat('*')
+				consoView.innerText = currentStack}
+})
+
+document.querySelector('.subtract').addEventListener('click', () => {
+	if (isNaN(parseInt(currentStack.charAt(currentStack.length-1))) == false){
+				currentStack = currentStack.concat('-')
+				consoView.innerText = currentStack}
+})
+
+document.querySelector('.add').addEventListener('click', () => {
+	if (isNaN(parseInt(currentStack.charAt(currentStack.length-1))) == false){
+				currentStack = currentStack.concat('+')
+				consoView.innerText = currentStack}
+})
+
+
+document.querySelector(`.lp`).addEventListener('click', () =>
+	 {currentStack = currentStack.concat('('); 
+		consoView.innerText = currentStack})
+
+document.querySelector(`.rp`).addEventListener('click', () =>
+		{let lp = 0, rp = 0
+		for (let x of currentStack){
+			if (x==='(') lp++
+			else if (x===')') rp++
+		}
+		if (rp < lp){
+		currentStack = currentStack.concat(')'); 
+		consoView.innerText = currentStack}})
+
+document.querySelector('.equals').addEventListener('click', () => {
+	let expression = currentStack
+	expression = expression.replace(/\^/g, '**')
+	let ans = eval(expression)
+	currentStack = ans.toString()
+	consoView.innerText = ans.toString()
+})
